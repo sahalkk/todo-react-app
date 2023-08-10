5cfb92bf9eb3f2706bc8eb23e0a5e2ed44e92e4d
sahalkk <msahalkk357@gmail.com>
2023-08-10 15:22:29 +0530

    todoform component changes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,50 +1,61 @@
-import { useEffect, useState } from "react"
-import TodoForm from "./TodoForm"
+import { useEffect, useState } from "react";
+import TodoForm from "./TodoForm";
 
-function App(){
-  const [todos, setTodos] = useState([])
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  function addTodo(title) {
+    setTodos((currentTodos) => {
+      return [...todos, { id: crypto.randomUUID(), title, completed: false }];
+    });
+  }
 
   function toggleTodo(id, status) {
-
-    let tempTodos = todos.map(todo => {
-      if(todo.id === id) {
-        todo.completed = status
-        return {...todo, status}
+    let tempTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = status;
+        return { ...todo, status };
+      } else {
+        return todo;
       }
-      else{
-        return todo
-      }
-    })
-    setTodos(tempTodos)
+    });
+    setTodos(tempTodos);
   }
 
   function deleteTodo(id) {
-    let tempTodos = todos.filter(itm => itm.id != id)
-    setTodos(tempTodos)
+    let tempTodos = todos.filter((itm) => itm.id != id);
+    setTodos(tempTodos);
   }
 
   return (
     <>
-    <TodoForm />
-    <h1 className="header">
-      ToDo List
-    </h1>
-    <ul className="list">
-      {todos.length===0 && "No ToDos"}
-      {todos.map(todo => {
-        return (
-        <li key={todo.id}>
-        <label htmlFor="">
-          <input type="checkbox" checked={todo['completed']} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
-          <span>{todo.title}</span>
-        </label>
-        <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
-      </li>
-        )
-      })}
-    </ul>
+      <TodoForm onSubmit={addTodo} />
+      <h1 className="header">ToDo List</h1>
+      <ul className="list">
+        {todos.length === 0 && "No ToDos"}
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label htmlFor="">
+                <input
+                  type="checkbox"
+                  checked={todo["completed"]}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
+                <span>{todo.title}</span>
+              </label>
+              <button
+                onClick={() => deleteTodo(todo.id)}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite-project/src/TodoForm.jsx
+++ vite-project/src/TodoForm.jsx
@@ -1,25 +1,31 @@
-import { useState } from "react"
+import { useState } from "react";
 
-
-function TodoForm() {
-
-  const [newItem, setNewItem]  = useState("")
+function TodoForm(props) {
+  const [newItem, setNewItem] = useState("");
 
   function handleSubmit(e) {
-    e.preventDefault()
-    // setTodos([...todos, {id:crypto.randomUUID(), title:newItem, completed:false }])
-    setNewItem("")
-    }
+    e.preventDefault();
+
+    if (newItem === "") return;
+    props.onSubmit(newItem);
+
+    setNewItem("");
+  }
 
   return (
     <form onSubmit={handleSubmit} className="new-item-form">
       <div className="form-row">
         <label htmlFor="item">New Item</label>
-        <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"/>
+        <input
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+          type="text"
+          id="item"
+        />
       </div>
       <button className="btn">Add</button>
     </form>
-  )
+  );
 }
 
 export default TodoForm;
